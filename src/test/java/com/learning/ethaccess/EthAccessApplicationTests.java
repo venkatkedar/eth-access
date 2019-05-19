98cf3131761f87bb3222f9e72ef27ce733e6543c
Kedar <venkat.kedar@gmail.com>
2019-05-19 13:35:21 -0700

    eth

---
+++ src/test/java/com/learning/ethaccess/EthAccessApplicationTests.java
@@ -0,0 +1,16 @@
+package com.learning.ethaccess;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EthAccessApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
